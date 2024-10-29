2f35a7770af0d746ccf1274e060e924e22b42690
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-10-29 12:35:02 -0300

    criando o projeto

---
+++ src/main/java/com/architrack/architrack/ArchitrackApplication.java
@@ -0,0 +1,13 @@
+package com.architrack.architrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/architrack/architrack/ArchitrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architrack.architrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
