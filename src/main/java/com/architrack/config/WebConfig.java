982ebb7e52d55ff1546bf0a2cc1bdddc419a3cfe
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-10-29 16:45:21 -0300

    criação do content negotiation(xml e yml) e cors

---
+++ src/main/java/com/architrack/config/WebConfig.java
@@ -0,0 +1,61 @@
+package com.architrack.config;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.architrack.contenttype.yml.YamlAbstractJackson2HttpMessage;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Value("${cors.originpatterns:default}")
+	private String originPatterns = "";
+	/*@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	
+		configurer.favorParameter(true)
+		.parameterName("MediaType")
+		.ignoreAcceptHeader(true)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);
+		
+	}*/
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json",MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlAbstractJackson2HttpMessage() );
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = originPatterns.split(",");
+		registry.addMapping("/**")
+		.allowedMethods("*")
+		.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
+	}
+	
+	
+	
+	
+}
