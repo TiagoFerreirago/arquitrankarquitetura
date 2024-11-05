4c693de441fa34d96c2329cb3c79187e155b0ecf
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-11-05 19:24:50 -0300

    criando configuração e teste de integração com testcontaines(swagger)

---
+++ src/test/java/com/architrack/integrationtest/tescontainers/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.architrack.integrationtest.tescontainers.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTest extends AbstractIntegrationTest{
+
+	@Test
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(8888)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
+	}
+
+}
